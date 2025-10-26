0d0f7bc594e8c001105be8947204347d7842c5b7
Francisco Montalvao <f.montalvao@outlook.com>
2025-10-26 19:38:11 -0300

    update ProductDTO for record

--- src/main/java/devsuperior/dscommerce/dto/ProductDTO.java
+++ src/main/java/devsuperior/dscommerce/dto/ProductDTO.java
@@ -1,54 +1,21 @@
 package devsuperior.dscommerce.dto;
 
-import devsuperior.dscommerce.entities.Category;
-import devsuperior.dscommerce.entities.OrderItem;
 import devsuperior.dscommerce.entities.Product;
 
-import java.util.HashSet;
-import java.util.Set;
+public record ProductDTO(
+        Long id,
+        String name,
+        String description,
+        Double price,
+        String imgUrl) {
 
-public class ProductDTO {
-    private Long id;
-    private String name;
-    private String description;
-    private Double price;
-    private String imgUrl;
-    //private Set<Category> categories = new HashSet<>();
-    //private Set<OrderItem> items = new HashSet<>();
-
-    public ProductDTO(){}
-    public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.imgUrl = imgUrl;
-    }
-    public ProductDTO(Product entity){
-        id= entity.getId();
-        name = entity.getName();
-        description = entity.getDescription();
-        price = entity.getPrice();
-        imgUrl = entity.getImgUrl();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public String getImgUrl() {
-        return imgUrl;
+    public ProductDTO(Product entity) {
+        this(
+                entity.getId(),
+                entity.getName(),
+                entity.getDescription(),
+                entity.getPrice(),
+                entity.getImgUrl()
+        );
     }
 }
